ccdab6646852396542592b7633a16f6a228162ea
Yuval Shafrir <10yuval10@gmail.com>
2022-05-09 14:03:58 +0300

    auth service delete unneeded line

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -5,8 +5,7 @@ async function login(email, phonenumber) {
   logger.debug(`auth.service - login with username: ${email}`)
   const user = await userService.getByUserEmail(email)
   if (!user) return Promise.reject('Invalid email or phone number')
-  // TODO: un-comment for real login
-  if (user.phonenumber !== phonenumber&&!user.isAdmin) return Promise.reject('Wrong credentials')
+  if (user.phonenumber !== phonenumber && !user.isAdmin) return Promise.reject('Wrong credentials')
   user._id = user._id.toString()
   return user
 }
